e6af62b4144c44f1449b07ea90a6ff884ba781aa
tamami <tamami.oka@gmail.com>
2015-10-05 16:10:51 +0700

    menambahkan parameter tahun pajak

--- src/main/java/lab/aikibo/manager/SpptManager.java
+++ src/main/java/lab/aikibo/manager/SpptManager.java
@@ -1,7 +1,9 @@
 package lab.aikibo.manager;
 
+import java.util.LinkedList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
@@ -11,11 +13,13 @@ import lab.aikibo.util.FormattedNop;
 import lab.aikibo.util.HibernateUtil;
 
 public class SpptManager {
+	Logger logger = Logger.getLogger(SpptManager.class);
 	
-	public List<Sppt> getDaftarSpptFormatBpd(List<String> daftarNop) {
+	public List<Sppt> getDaftarSpptFormatBpd(List<String> daftarNop, String thnPajak) {
 		Session session = (Session) HibernateUtil.getSessionFactory();
 		
 		session.beginTransaction();
+		List<Sppt> result = new LinkedList<Sppt>();
 		for(int i=0; i<daftarNop.size(); i++) {
 			FormattedNop formattedNop = new FormattedNop(daftarNop.get(i));
 			Criteria criteria = session.createCriteria(Sppt.class);
@@ -24,8 +28,19 @@ public class SpptManager {
 			criteria.add(Restrictions.eq("kdDati2", formattedNop.getKdDati2()));
 			criteria.add(Restrictions.eq("kdKecamatan", formattedNop.getKdKecamatan()));
 			criteria.add(Restrictions.eq("kdKelurahan", formattedNop.getKdKelurahan()));
+			criteria.add(Restrictions.eq("kdBlok", formattedNop.getKdBlok()));
+			criteria.add(Restrictions.eq("noUrut", formattedNop.getNoUrut()));
+			criteria.add(Restrictions.eq("kdJnsOp", formattedNop.getKdJnsOp()));
+			criteria.add(Restrictions.eq("thnPajakSppt", thnPajak));
+			
+			List<Sppt> data = (List<Sppt>) criteria.list();
+			if(data.size() > 0) {
+				result.add((Sppt)data.get(0));
+			} else {
+				logger.error("Data nop : " + formattedNop.getFormattedNop());
+			}
 		}
-		return null;
+		return result;
 	}
 
 }
