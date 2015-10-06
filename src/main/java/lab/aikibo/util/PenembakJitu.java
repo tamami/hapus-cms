a393eb6aeeccdedc2924f14b51d3c74b28c4929e
tamami <tamami.oka@gmail.com>
2015-10-06 15:58:12 +0700

    update untuk uji coba

--- src/main/java/lab/aikibo/util/PenembakJitu.java
+++ src/main/java/lab/aikibo/util/PenembakJitu.java
@@ -4,10 +4,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
+import lab.aikibo.entity.Sppt;
 import lab.aikibo.manager.SPManager;
+import lab.aikibo.manager.SpptManager;
 
 import org.apache.log4j.Logger;
 
@@ -16,7 +20,7 @@ public class PenembakJitu {
   
   public static boolean processFile(File dataFile) throws FileNotFoundException {
     Scanner scnFile = new Scanner(dataFile);
-    SPManager callSP = new SPManager();
+    SpptManager spptManager = new SpptManager();
     int lineNumber = 0;
     while(scnFile.hasNextLine()) {
       scnFile.nextLine();
@@ -26,15 +30,22 @@ public class PenembakJitu {
 
     scnFile = new Scanner(dataFile);
     int i=1;
+    List<String> data = new LinkedList<String>();
     while(scnFile.hasNextLine()) {
       Scanner scnData = new Scanner(scnFile.nextLine()).useDelimiter(Pattern.compile("\\|"));
+      String kdBpd = scnData.next();
       String nop = scnData.next();
       String thn = scnData.next();
+      String nama = scnData.next();
+      String pbb = scnData.next();
+      String denda = scnData.next();
       
-      logger.info(nop + " - " + thn + " diproses");
+      data.add(nop);
+      
+      logger.info(nop + " - " + thn + " dibaca");
     }
     
-    callSP.finish();
+    List<Sppt> resultData = spptManager.getDaftarSpptFormatBpd(data, "2015");
     return true;
   }
 }
